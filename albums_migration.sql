ef6f5190a9c2ce4ac18d55e67287884231fb0d5d
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-24 21:39:01 -0600

    working on the albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,9 +2,13 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist_name VARCHAR(50) DEFAULT,
+    artist_name VARCHAR(100) NOT NULL,
     record_name VARCHAR(100) NOT NULL,
     release_date INT NOT NULL,
     sales FLOAT NOT NULL,
-    genre VARCHAR
-)
+    genre VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+
