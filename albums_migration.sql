f36fb84c37e7191f6a110d217b92461928f61784
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-24 23:07:31 -0600

    completed database exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,9 +3,7 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-
     artist_name VARCHAR(100) NOT NULL,
-
     record_name VARCHAR(100) NOT NULL,
     release_date SMALLINT UNSIGNED,
     sales FLOAT NOT NULL,
