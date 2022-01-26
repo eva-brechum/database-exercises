8dbc82a6df4137452198d3dc05efa8b0147091d9
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-26 13:28:21 -0600

    finished Part One of the where clauses exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,10 +1,11 @@
 USE codeup_test_db;
 
-SELECT *  FROM albums WHERE artist_name = 'Pink Floyd'; --displays 'The Dark Side of the Moon and The Wall
+SELECT *  FROM albums WHERE artist_name = 'Pink Floyd';
+# --displays 'The Dark Side of the Moon and The Wall--
 
 SELECT release_date FROM albums WHERE artist_name= 'Sgt Pepper''s Lonely Hearts Club Band.';
 
-SELECT genre FROM albums WHERE artist_name =
+# SELECT genre FROM albums WHERE artist_name =
 
 
 
