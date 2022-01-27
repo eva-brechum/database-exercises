ed0a3ae2db459b6435013dbfda6502ebe634bf87
Eva Brechum <Brechum.Eva@gmail.com>
2022-01-27 15:34:23 -0600

    finished six problems in the groupby exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -12,14 +12,26 @@ GROUP BY last_name;
 
 -- Update your previous query to now find unique combinations of first and lastname where the last name starts and ends with 'E'.
 -- You should get 846 rows. --
-SELECT last_name
+SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%E'
-GROUP BY last_name;
+GROUP BY first_name, last_name;
 
 -- Find the unique last names with a 'q' but not 'qu'. Your results should be:
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND
+        last_name  NOT LIKE  '%qu%'
+GROUP BY last_name;
 
 -- Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+SELECT COUNT(last_name), last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND
+        last_name  NOT LIKE  '%qu%'
+GROUP BY last_name
+ORDER BY COUNT(last_name) ASC;
+# needs to be fixed
 
 -- Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names. Your results should be:
 
