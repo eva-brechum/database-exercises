b7d8382656973d04fb349c96226d42f67841ee4d
Eva Brechum <Brechum.Eva@gmail.com>
2022-02-01 10:38:52 -0600

    finished first bonus of subqueries exercise and working on second bonus

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -30,11 +30,24 @@ WHERE emp_no IN
         WHERE to_date > curdate() AND employees.gender = 'F'
     );
 
--- Find all the department names that currently have female managers
+-- BONUS 1: Find all the department names that currently have female managers
 SELECT dept_name
 FROM departments
 WHERE dept_no IN
       (SELECT dept_no
           FROM dept_manager
-          WHERE to_date >curdate() AND emp_no
-    )
+          WHERE to_date >curdate() AND emp_no IN(
+              SELECT emp_no
+              FROM employees
+              WHERE gender = 'F'
+              )
+    );
+
+ -- BONUS 2. Find the first and last name of the employee with the highest salary.
+ SELECT first_name, last_name
+ FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM salaries
+    WHERE salary
+    );
