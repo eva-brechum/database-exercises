7ad613e170bf411969617613fd1b41e2dd7607a0
Eva Brechum <Brechum.Eva@gmail.com>
2022-02-21 15:51:21 -0600

    updated select exercises to write queries to find given information

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,11 +1,28 @@
 USE codeup_test_db;
 
-SELECT *  FROM albums WHERE artist_name = 'Pink Floyd';
+SELECT record_name FROM albums WHERE artist_name = 'Pink Floyd';
 # --displays 'The Dark Side of the Moon and The Wall--
 
 SELECT release_date FROM albums WHERE artist_name= 'Sgt Pepper''s Lonely Hearts Club Band.';
 
-# SELECT genre FROM albums WHERE artist_name =
+SELECT genre FROM albums WHERE artist_name = 'Nevermind';
+
+SELECT * FROM albums WHERE release_date BETWEEN 1989 AND 2000;
+
+
+SELECT record_name FROM albums WHERE sales <20;
+
+SELECT record_name FROM albums WHERE genre = 'Rock'
+
+
+
+
+
+
+
+
+
+
 
 
 
